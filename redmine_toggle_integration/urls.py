dd02ad0c8941269ec879b0748c6f45dc419755dc
David Taing <dtaing1@uci.edu>
2023-01-02 11:40:54 -0800

    test app completed

--- redmine_toggle_integration/urls.py
+++ redmine_toggle_integration/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from test_ap import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path('', include('hello_world.urls')),
+    path('', views.hello_world, name='hello_world'),
 ]
